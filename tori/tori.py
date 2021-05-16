8e00316b2a62927866d04af39c116873404d0e5c
AidenH <aidenalexh@gmail.com>
2021-05-16 19:57:10 -0300

    First connectivity to exchange: single trade event log.

--- tori/tori.py
+++ tori/tori.py
@@ -1,14 +1,40 @@
 import websockets
 import tkinter as tk
-import binance_f
 
+from binance_f import SubscriptionClient
+from binance_f.constant.test import *
+from binance_f.model import *
+from binance_f.exception.binanceapiexception import BinanceApiException
+from binance_f.base.printobject import *
+
+import keys
+
+#setup
 window = tk.Tk()
 window.geometry("400x700")
 window.attributes('-topmost', True)
 
+sub_client = SubscriptionClient(api_key=keys.api, secret_key=keys.secret)
+
+#funcs
 def connect():
     print("test")
+    sub_client.subscribe_aggregate_trade_event("btcusdt", callback, error)
 
+def callback(data_type: 'SubscribeMessageType', event: 'any'):
+    if data_type == SubscribeMessageType.RESPONSE:
+        print("EventID: ", event)
+    elif data_type == SubscribeMessageType.PAYLOAD:
+        PrintBasic.print_obj(event)
+        sub_client.unsubscribe_all()
+    else:
+        print("Unknown Data:")
+    print()
+
+def error(e: 'BinanceApiException'):
+    print(e.error_code + e.error_message)
+
+#tkinter
 subbutton = tk.Button(
     command = connect,
     text = "Subscribe",
